2910fdadf90860d54e2277916afae168cad90764
Emma Gunnari <emma.gunnari@iths.se>
2022-04-23 15:16:51 +0200

    second initial config dbaccess

---
+++ DataAccess/Models/PerfumeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccess.Models;
+
+internal class PerfumeModel
+{
+    public int Id { get; set; }
+    public string Name  { get; set; }
+    public string Designer { get; set; }
+    public string Perfumer { get; set; }
+    public int Launched { get; set; }
+    public string Description { get; set; }
+}
