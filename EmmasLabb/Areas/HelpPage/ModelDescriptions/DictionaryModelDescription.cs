c90c6de0f264d37970b79fc9305a7056474d1a30
Emma Gunnari <emma.gunnari@iths.se>
2022-04-26 10:09:43 +0200

    final changes commit

--- EmmasLabb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EmmasLabb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- EmmasLabb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmmasLabb.WebApiApplication" Language="C#" %>
